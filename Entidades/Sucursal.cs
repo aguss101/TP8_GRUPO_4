95da5e1703d190a10184cd9dd7ad1752d84fd21e
Luca Gauna <83098830+lucagauna@users.noreply.github.com>
2025-05-29 13:29:24 -0300

    clase sucursal
    - Ahora voy a hacer la conexion con los datos.

---
+++ Entidades/Sucursal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Sucursal
+    {
+        public int IdSucursal { get; set; }
+        public string NombreSucursal { get; set; }
+        public string DireccionSucursal { get; set; }
+        public string DescripcionSucursal { get; set; }
+        public int idProvinciaSucursal { get; set; }
+    }
+}
